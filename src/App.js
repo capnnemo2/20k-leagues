6a2111f6b6d8cb196560821aef9f7d08ac688b97
Ben Hernandez <benjammin234@gmail.com>
2020-05-03 15:39:14 -0700

    Adds basic wireframe styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>Nav</nav>
+      <main>
+        <header>
+          <h1>Hero</h1>
+        </header>
+
+        <section>Section 1</section>
+
+        <section>Section 2</section>
+
+        <section>Section 3</section>
+
+        <section>Section 4</section>
+      </main>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 export default App;
