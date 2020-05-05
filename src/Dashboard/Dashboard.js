303385c8c42da9de61c24dec6044d1dc3ac76a4c
Ben Hernandez <benjammin234@gmail.com>
2020-05-05 09:22:30 -0700

    Adds dive stats fns

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -4,7 +4,105 @@ import "./Dashboard.css";
 import dummyStore from "../dummyStore";
 
 export default class Dashboard extends React.Component {
+  findDeepestDive(arr) {
+    let diveDepths = [];
+    for (let i = 0; i < arr.length; i++) {
+      diveDepths.push(arr[i].maxDepth);
+    }
+    return (
+      diveDepths.reduce(function (prevLargestNum, currLargestNum) {
+        return currLargestNum > prevLargestNum
+          ? currLargestNum
+          : prevLargestNum;
+      }) + " ft."
+    );
+  }
+
+  findAvgDiveDepth(arr) {
+    let diveDepths = [];
+    for (let i = 0; i < arr.length; i++) {
+      diveDepths.push(arr[i].maxDepth);
+    }
+    let sum = diveDepths.reduce((a, b) => a + b, 0);
+    let avg = sum / arr.length;
+    return avg + " ft.";
+  }
+
+  findShallowestDive(arr) {
+    let diveDepths = [];
+    for (let i = 0; i < arr.length; i++) {
+      diveDepths.push(arr[i].maxDepth);
+    }
+    return (
+      diveDepths.reduce(function (prevSmallestNum, currSmallestNum) {
+        return currSmallestNum < prevSmallestNum
+          ? currSmallestNum
+          : prevSmallestNum;
+      }) + " ft."
+    );
+  }
+
+  findLongestDive(arr) {
+    let diveTimes = [];
+    for (let i = 0; i < arr.length; i++) {
+      diveTimes.push(arr[i].duration);
+    }
+    return (
+      diveTimes.reduce(function (prevLargestNum, currLargestNum) {
+        return currLargestNum > prevLargestNum
+          ? currLargestNum
+          : prevLargestNum;
+      }) + " min."
+    );
+  }
+
+  findAvgDiveTime(arr) {
+    let diveTimes = [];
+    for (let i = 0; i < arr.length; i++) {
+      diveTimes.push(arr[i].duration);
+    }
+    let sum = diveTimes.reduce((a, b) => a + b, 0);
+    let avg = sum / arr.length;
+    return avg + " min.";
+  }
+
+  findShortestDive(arr) {
+    let diveTimes = [];
+    for (let i = 0; i < arr.length; i++) {
+      diveTimes.push(arr[i].duration);
+    }
+    return (
+      diveTimes.reduce(function (prevSmallestNum, currSmallestNum) {
+        return currSmallestNum < prevSmallestNum
+          ? currSmallestNum
+          : prevSmallestNum;
+      }) + " min."
+    );
+  }
+
+  findTotalDiveTime(arr) {
+    let diveTimes = [];
+    for (let i = 0; i < arr.length; i++) {
+      diveTimes.push(arr[i].duration);
+    }
+    const sum = diveTimes.reduce((a, b) => a + b, 0);
+    return this.convertTime(sum);
+  }
+
+  convertTime(num) {
+    let days = Math.floor(num / 1440);
+    console.log(days);
+    let hours = Math.floor((num - days * 1440) / 60);
+    let minutes = num % 60;
+    if (days > 0) {
+      return days + " days : " + hours + " hr : " + minutes + " min.";
+    } else {
+      return hours + " hr : " + minutes + " min.";
+    }
+  }
+
   render() {
+    const totalDives = dummyStore.dives.length;
     return (
       <div className="Dashboard">
         <section>
@@ -64,21 +162,30 @@ export default class Dashboard extends React.Component {
           time under water deepest dive, longest dive, etc.
           <h2>Dive Stats</h2>
           <ul>
-            <li>Total Dives: </li>
+            <li>Total Dives: {totalDives}</li>
             <li>
               <ul>
-                <li>Deepest Dive:</li>
-                <li>Avg Dive Depth:</li>
-                <li>Shallowest Dive:</li>
+                <li>Deepest Dive: {this.findDeepestDive(dummyStore.dives)}</li>
+                <li>
+                  Avg Dive Depth: {this.findAvgDiveDepth(dummyStore.dives)}
+                </li>
+                <li>
+                  Shallowest Dive: {this.findShallowestDive(dummyStore.dives)}{" "}
+                </li>
               </ul>
             </li>
             <br />
             <li>
               <ul>
-                <li>Longest Dive:</li>
-                <li>Avg Dive Time:</li>
-                <li>Shortest Dive:</li>
-                <li>Total Time Spent Underwater:</li>
+                <li>Longest Dive: {this.findLongestDive(dummyStore.dives)}</li>
+                <li>Avg Dive Time: {this.findAvgDiveTime(dummyStore.dives)}</li>
+                <li>
+                  Shortest Dive: {this.findShortestDive(dummyStore.dives)}
+                </li>
+                <li>
+                  Total Time Spent Underwater:{" "}
+                  {this.findTotalDiveTime(dummyStore.dives)}
+                </li>
               </ul>
             </li>
           </ul>
