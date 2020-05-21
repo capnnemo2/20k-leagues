3c3fe850c5080b5cb33d2dd282840b6900f6cf14
Ben Hernandez <benjammin234@gmail.com>
2020-05-21 09:58:17 -0700

    Changes Nav link order

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -16,8 +16,8 @@ export default class Nav extends React.Component {
       <div className="nav-container">
         <Link to="/">20k Leagues</Link>
         <Link to="/animal-finder">Animal Finder</Link>
-        <Link to="/profile">Profile</Link>
         <Link to="/log">Log</Link>
+        <Link to="/profile">Profile</Link>
         <Link to="/" onClick={() => this.handleLogOut()}>
           Logout
         </Link>
