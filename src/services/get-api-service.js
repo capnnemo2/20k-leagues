7b27b4c5b0a9367236ef846c408f478f9e5d908b
Ben Hernandez <benjammin234@gmail.com>
2020-06-02 12:48:04 -0700

    Continues creating api service files, need checking

---
+++ src/services/get-api-service.js
@@ -0,0 +1,81 @@
+import config from "../config";
+import TokenService from "./token-service";
+
+const GetApiService = {
+  getCountries() {
+    return fetch(`${config.API_ENDPOINT}/countries`, {
+      headers: {},
+    }).then((res) =>
+      !res.ok ? res.json((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  getAnimals() {
+    return fetch(`${config.API_ENDPOINT}/animals`, {
+      headers: {},
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  getSpecialties() {
+    return fetch(`${config.API_ENDPOINT}/specialties`, {
+      headers: {},
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  // if this function is implemented in the CDM fn of App component, will the animal tracker list re-render after addAnimalTracked() runs? that fn should return the new animals tracked list, right?
+  getAnimalsTracked() {
+    return fetch(`${config.API_ENDPOINT}/animalTracker`, {
+      headers: {},
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+
+  // do I get the user by id or by email?
+  getUser(userId) {
+    // return fetch(`${config.API_ENDPOINT}/${userId}`);
+  },
+
+  getUserCerts(userId) {
+    return fetch(`${config.API_ENDPOINT}/certs/user/${userId}`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  // OR -----------
+  getAllCerts() {
+    return fetch(`${config.API_ENDPOINT}/certs`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+
+  getUserDives(userId) {
+    return fetch(`${config.API_ENDPOINT}/dives/user/${userId}`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  // OR ----------
+  getAllDives() {
+    return fetch(`${config.API_ENDPOINT}/dives`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+};
+
+export default GetApiService;
---
+++ src/services/non-get-api-service.js
@@ -0,0 +1,129 @@
+import config from "../config";
+import TokenService from "./token-service";
+
+const nonGetApiService = {
+  // USERS
+  addUser(newUser) {
+    fetch(`${config.API_ENDPOINT}/users`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newUser,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  updateUser(userId, newUser) {
+    fetch(`${config.API_ENDPOINT}/users/${userId}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newUser,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  //   DIVES
+  addDive(newDive) {
+    fetch(`${config.API_ENDPOINT}/dives`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newDive,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  updateDive(diveId, newDive) {
+    fetch(`${config.API_ENDPOINT}/dives/${diveId}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newDive,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  deleteDive(diveId) {
+    fetch(`${config.API_ENDPOINT}/dives/${diveId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  //   CERTS
+  addCert(newCert) {
+    fetch(`${config.API_ENDPOINT}/certs`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newCert,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  deleteCert(certId) {
+    fetch(`${config.API_ENDPOINT}/certs/${certId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  //   ANIMAL TRACKER
+  addAnimalTracked(newAnimalTracked) {
+    fetch(`${config.API_ENDPOINT}/animalTracker`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newAnimalTracked,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  updateAnimalsTracked(animal, region) {
+    fetch(`${config.API_ENDPOINT}/animalTracker/${diveId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({ animal, region }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+};
+
+export default nonGetApiService;
